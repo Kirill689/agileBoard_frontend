0966eb201ce112cbb510c60c1c1a2a2487ecacf9
Kirill Tseitlin <Kirillphonelg@gmail.com>
2019-11-18 21:35:54 +0200

    Basic designed components and routes added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import AgileBoard from './components/AgileBoard';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddTask from './components/task/AddTask';
+import UpdateTask from './components/task/UpdateTask';
 
 class App extends Component {
 
   render(){
     return(
+      <Router>
       <div className="App">
-        <h1>Hello Agile</h1>
+        <NavBar/>
+        <Route exact path="/" component={AgileBoard}/>
+        <Route exact path="/addTask" component={AddTask}/>
+        <Route exact path="/updateTask" component={UpdateTask}/>
       </div>
+      </Router>
     )
   }
 
