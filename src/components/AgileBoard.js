fafc986ce4a0cfc70b9faefbf4d84eea00b0d7fa
Kirill Tseitlin <Kirillphonelg@gmail.com>
2019-12-01 22:35:37 +0200

    Add Task Component created

--- src/components/AgileBoard.js
+++ src/components/AgileBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import{ Link } from "react-router-dom";
+import TaskItem from './task/TaskItem';
 
 export default class AgileBoard extends Component {
     render() {
@@ -14,6 +15,8 @@ export default class AgileBoard extends Component {
 
             <div className="container">
                 <div className="row">
+
+
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
                             <div className="card-header bg-secondary text-white">
@@ -21,49 +24,35 @@ export default class AgileBoard extends Component {
                             </div>
                         </div>
     
-                     
-                        <div className="card mb-1 bg-light">
-    
-                            <div className="card-header text-primary">
-                                ID: projectSequence
-                            </div>
-                            <div className="card-body bg-light">
-                                <h5 className="card-title">summary</h5>
-                                <p className="card-text text-truncate ">
-                                    acceptanceCriteria
-                                </p>
-                                <a href="" className="btn btn-primary">
-                                    View / Update
-                                </a>
-    
-                                <button className="btn btn-danger ml-4">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-    
-                        {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
+                        <TaskItem></TaskItem>
+                      
                     </div>
+
+
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
                             <div className="card-header bg-primary text-white">
                                 <h3>In Progress</h3>
                             </div>
                         </div>
-                        {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
-    
-                        {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
+                        
+                        <TaskItem></TaskItem>
+
                     </div>
+
+
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
                             <div className="card-header bg-success text-white">
                                 <h3>Done</h3>
                             </div>
                         </div>
-                        {/* <!-- SAMPLE PROJECT TASK STARTS HERE --> */}
-    
-                        {/* <!-- SAMPLE PROJECT TASK ENDS HERE --> */}
+                       
+                        <TaskItem></TaskItem>
+                        
                     </div>
+
+
                 </div>
             </div>
       
--- src/components/task/AddTask.js
+++ src/components/task/AddTask.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react'
 import{ Link } from "react-router-dom";
 
 export default class AddTask extends Component {
+
+    constructor(){
+
+        super();
+
+        this.state = {
+            summary:'',
+            acceptanceCriteria:'',
+            status:''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    };
+
+
+    onChange(e){
+        this.setState({ [e.target.name] : e.target.value}); 
+    };
+
+    onSubmit(e){
+        e.preventDefault();
+        const newTask = {
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status
+        }
+
+        console.log(newTask);
+        
+ 
+    }
+
+
     render() {
         return (
             <div className="addProjectTask">
@@ -12,23 +47,45 @@ export default class AddTask extends Component {
                         Back to Board
                     </Link>
                     <h4 className="display-4 text-center">Add /Update Project Task</h4>
-                    <form>
+
+                    <form onSubmit={this.onSubmit} >
+
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                            <input 
+                            type="text" 
+                            className="form-control form-control-lg" 
+                            name="summary" 
+                            value={this.state.taskSummary} 
+                            onChange={this.onChange} 
+                            placeholder="Task Summary"
+                            />
                         </div>
+
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                            <textarea 
+                            className="form-control form-control-lg"  
+                            value={this.state.taskAcceptance} 
+                            onChange={this.onChange} 
+                            name="acceptanceCriteria"
+                            placeholder="Acceptance Criteria"></textarea>
                         </div>
+
                         <div className="form-group">
-                            <select className="form-control form-control-lg" name="status">
+                            <select 
+                            className="form-control form-control-lg" 
+                            name="status" 
+                            value={this.state.taskStatus}
+                            onChange={this.onChange}>
                                 <option value="">Select Status</option>
                                 <option value="TO_DO">TO DO</option>
                                 <option value="IN_PROGRESS">IN PROGRESS</option>
                                 <option value="DONE">DONE</option>
                             </select>
                         </div>
-                        <input type="submit" className="btn btn-primary btn-block mt-4" />
-                    </form>
+
+                        <input type="submit" className="btn btn-primary btn-block mt-4"/>
+
+                    </form> 
                 </div>
             </div>
         </div>
